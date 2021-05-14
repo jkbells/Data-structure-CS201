7cad28f6db66a192aaa16e69c6f00e32512102dc
jawad khan <73393333+Mythical-stack@users.noreply.github.com>
2021-05-14 11:05:23 +0500

    initial comment

---
+++ Practice code/Linked list/Queue/Max Consecutive Ones/code.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    vector<int> a(n);
+    for (auto &i : a)
+    cin >> i;
+    int zerocount = 0, i =0, ans = 0;
+    for (int j=0;j<n;j++){
+        if(a[j] == 0)
+        zerocount++;
+        while(zerocount > k){
+            if(a[i] == 0){
+                zerocount--;
+            }
+            i++;
+        }
+        ans = max(ans,j-i+1);
+    }
+    cout << ans;
+    return 0;
+}
+
+ 
