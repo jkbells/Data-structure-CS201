0742cf2dd09e2dfc313ddb4343a9b5426b2d0b55
jawad khan <73393333+MythicalStack@users.noreply.github.com>
2021-06-28 17:48:02 +0500

    initial comment

---
+++ Practice code/Linked list/Graphs/Cycle Detection in Undirected Graphs/cycle.cpp
@@ -0,0 +1,43 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+bool iscycle(int src,vector<vector<int>> &adj,vector<bool> &visited,int parent){
+    visited[src] = true;
+    for(auto i : adj[src]){
+        if(1!=parent){
+            if(visited[i])
+            return true;
+            if(!visited[i] and iscycle(i,adj,visited,src)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n);
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bool cycle = false;
+    vector<bool> visited(n,false);
+    for(int i=0;i<n;i++){
+        if(!visited[i] and iscycle(i,adj,visited,-1)){
+            cycle = true;
+        }
+    }
+    if(cycle){
+        cout << "cycle is present" << endl;
+    }
+    else{
+        cout << "cycle is not present" <<endl;
+    }
+}
